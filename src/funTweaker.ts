81604b400b0f876277ab4ecb065bef971e344db8
Defucilis <defucilis@gmail.com>
2021-04-15 17:26:16 +1000

    feat: Add FunLimiter functionality

--- src/funTweaker.ts
+++ src/funTweaker.ts
@@ -21,3 +21,49 @@ export const getRemappedScript = (funscript: Funscript, min: number, max: number
         return {...action, pos: Math.max(0, Math.min(100, Math.round(newPos)))}
     })};
 }
+/**
+ * Limits a funscript's position values to ensure that it doesn't exceed a given maximum speed value
+ * @param  {Funscript} funscript - The funscript to limit
+ * @param  {"launch"|"handy"|number} maxSpeed - The time, in milliseconds, it takes for the device to complete one full movement (one-direction). Known device values are included as string options
+ * @returns The limited funscript
+ */
+export const getLimitedScript = (
+    funscript: Funscript, 
+    maxSpeed: "launch" | "handy" | number
+): Funscript => {
+    if(!funscript || !funscript.actions || funscript.actions.length === 0) return funscript;
+
+    let fullStrokeDuration: number = 0;
+    switch(maxSpeed) {
+        case "launch":
+            fullStrokeDuration = 265;
+            break;
+        case "handy":
+            fullStrokeDuration = 231;
+            break;
+        default:
+            fullStrokeDuration = maxSpeed;
+             break;
+    }
+    const maxPossibleSpeed = 100 / fullStrokeDuration;
+
+    const newActions: Action[] = [funscript.actions[0]];
+    for(let i = 1; i < funscript.actions.length; i++) {
+        const action = funscript.actions[i];
+        const lastAction = newActions[i - 1];
+        const actionSpeed = getSpeed(action, lastAction);
+        if(actionSpeed <= maxPossibleSpeed) {
+            newActions.push(action);
+            continue;
+        }
+
+        let newPos = action.pos;
+        if(action.pos < lastAction.pos) {
+            newPos = lastAction.pos - maxPossibleSpeed * (action.at - lastAction.at)
+        } else {
+            newPos = lastAction.pos + maxPossibleSpeed * (action.at - lastAction.at)
+        }
+        newActions.push({...action, pos: newPos});
+    }
+    return {...funscript, actions: newActions};
+}
